e4a31cb07edce89e32d4445f78d6a9a898037381
sqin <sheng.qin@hotmail.com>
2020-04-13 23:37:07 +0800

    画出一个爆炸

---
+++ src/com/qinsheng/tank/Explode.java
@@ -0,0 +1,34 @@
+package com.qinsheng.tank;
+
+import java.awt.*;
+
+/**
+ * Created by qinsheng on 2020/4/12.
+ */
+public class Explode {
+
+    public static int WIDTH = ResourceManager.explodes[0].getWidth();
+    public static int HEIGHT = ResourceManager.explodes[0].getHeight();
+
+    private int x, y;
+
+    TankFrame tankFrame = null;
+    private boolean living = true;
+
+    private int step = 0;
+
+    public Explode(int x, int y, TankFrame tankFrame) {
+        this.x = x;
+        this.y = y;
+        this.tankFrame = tankFrame;
+    }
+
+
+    public void paint(Graphics graphics) {
+
+        graphics.drawImage(ResourceManager.explodes[step++], x, y, null);
+        if(step >= ResourceManager.explodes.length){
+            step = 0;
+        }
+    }
+}
--- src/com/qinsheng/tank/TankFrame.java
+++ src/com/qinsheng/tank/TankFrame.java
@@ -17,7 +17,7 @@ public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 400, Dir.DOWN, Group.GOOD, this);
     List<Bullet> bulletList = new ArrayList<>();
     List<Tank> tankList = new ArrayList<>();
-//    Bullet bullet = new Bullet(200, 200, Dir.DOWN, this);
+    Explode explode = new Explode(100, 100, this);
 
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
@@ -62,6 +62,8 @@ public class TankFrame extends Frame {
 
 
         myTank.paint(graphics);
+
+        explode.paint(graphics);
 
 //        for(Bullet bullet : bulletList) {
 //            bullet.paint(graphics);
