fd37119b1c7c0a37d281a39411010714abbef7cb
sqin <sheng.qin@hotmail.com>
2020-04-14 00:20:52 +0800

    把爆炸声音弄出来

--- src/com/qinsheng/tank/Explode.java
+++ src/com/qinsheng/tank/Explode.java
@@ -21,6 +21,8 @@ public class Explode {
         this.x = x;
         this.y = y;
         this.tankFrame = tankFrame;
+
+        new Thread(() -> new Audio("audio/explode.wav").play()).start();
     }
 
 
