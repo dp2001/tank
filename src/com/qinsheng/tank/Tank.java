048f96eeae9c55506f34e3b8c81a62130e703c00
sqin <sheng.qin@hotmail.com>
2020-04-14 21:51:43 +0800

    边界检测

--- src/com/qinsheng/tank/Tank.java
+++ src/com/qinsheng/tank/Tank.java
@@ -137,6 +137,16 @@ public class Tank {
         if(this.group == Group.BAD && random.nextInt(100) > 95) {
             randomDir();
         }
+
+        //边界检测
+        boundsCheck();
+    }
+
+    private void boundsCheck() {
+        if(this.x < 0) x = 0;
+        if(this.y < 30) y = 30;
+        if(this.x > TankFrame.GAME_WIDTH - Tank.WIDTH) x = TankFrame.GAME_WIDTH - Tank.WIDTH;
+        if(this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT) y = TankFrame.GAME_HEIGHT - Tank.HEIGHT;
     }
 
     //随机方向
